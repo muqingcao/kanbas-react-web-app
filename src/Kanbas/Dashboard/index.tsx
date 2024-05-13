c3e86b75b8ded1c5d6386be1cbb34ff427bf5606
muqingcao <caomuqingy@gmail.com>
2024-05-13 09:40:37 -0700

    second commit

---
+++ src/Kanbas/Dashboard/index.tsx
@@ -0,0 +1,114 @@
+export default function Dashboard() {
+    return (
+        <div>
+            <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
+            <h2 id="wd-dashboard-published">Published Courses (12)</h2> <hr />
+            <div id="wd-dashboard-courses">
+                <div className="wd-dashboard-course">
+                    <img src="/images/5001.png" width={200} />
+                    <div>
+                        <a className="wd-dashboard-course-link"
+                            href="#/Kanbas/Courses/Home">
+                            CS5001 Intensive Foundations of CS
+                        </a>
+                        <p className="wd-dashboard-course-title">
+                            Full Stack software developer
+                        </p>
+                        <a href="#/Kanbas/Courses/Home"> 
+                        Go 
+                        </a>
+                    </div>
+                </div>
+                <div className="wd-dashboard-course">
+                <img src="/images/5002.png" width={200} />
+                    <div>
+                        <a className="wd-dashboard-course-link"
+                            href="#/Kanbas/Courses/Home">
+                            CS5002 Discrete Structures
+                        </a>
+                        <p className="wd-dashboard-course-title">
+                            Full Stack software developer
+                        </p>
+                        <a href="#/Kanbas/Courses/Home"> 
+                        Go 
+                        </a>
+                    </div>
+                </div>
+                <div className="wd-dashboard-course">
+                <img src="/images/5004.png" width={200} />
+                    <div>
+                        <a className="wd-dashboard-course-link"
+                            href="#/Kanbas/Courses/Home">
+                            CS5004 Object Oriented Design
+                        </a>
+                        <p className="wd-dashboard-course-title">
+                            Full Stack software developer
+                        </p>
+                        <a href="#/Kanbas/Courses/Home"> 
+                        Go 
+                        </a>
+                    </div>
+                </div>
+                <div className="wd-dashboard-course">
+                <img src="/images/5008.png" width={200} />
+                    <div>
+                        <a className="wd-dashboard-course-link"
+                            href="#/Kanbas/Courses/Home">
+                            CS5008 Data Structures, Algorithms, and Their Applications Within Computer Systems
+                        </a>
+                        <p className="wd-dashboard-course-title">
+                            Full Stack software developer
+                        </p>
+                        <a href="#/Kanbas/Courses/Home"> 
+                        Go 
+                        </a>
+                    </div>
+                </div>
+                <div className="wd-dashboard-course">
+                <img src="/images/5610.png" width={200} />
+                    <div>
+                        <a className="wd-dashboard-course-link"
+                            href="#/Kanbas/Courses/Home">
+                            CS5610 Web Development
+                        </a>
+                        <p className="wd-dashboard-course-title">
+                            Full Stack software developer
+                        </p>
+                        <a href="#/Kanbas/Courses/Home"> 
+                        Go 
+                        </a>
+                    </div>
+                </div>
+                <div className="wd-dashboard-course">
+                <img src="/images/5800.png" width={200} />
+                    <div>
+                        <a className="wd-dashboard-course-link"
+                            href="#/Kanbas/Courses/Home">
+                            CS5800 Algorithms
+                        </a>
+                        <p className="wd-dashboard-course-title">
+                            Full Stack software developer
+                        </p>
+                        <a href="#/Kanbas/Courses/Home"> 
+                        Go 
+                        </a>
+                    </div>
+                </div>
+                <div className="wd-dashboard-course">
+                <img src="/images/5520.png" width={200} />
+                    <div>
+                        <a className="wd-dashboard-course-link"
+                            href="#/Kanbas/Courses/Home">
+                            CS5520 Mobile Application Development
+                        </a>
+                        <p className="wd-dashboard-course-title">
+                            Full Stack software developer
+                        </p>
+                        <a href="#/Kanbas/Courses/Home"> 
+                        Go 
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+  );}
--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -3,5 +3,5 @@ export default function Lab2() {
         <div>
             <h2>Lab 2</h2>
         </div>
-    );
+    ); 
 }
