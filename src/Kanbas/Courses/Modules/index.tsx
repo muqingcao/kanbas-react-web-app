8f3194a626791ef5dfb1ec37b61c7b5d09009a6b
muqingcao <caomuqingy@gmail.com>
2024-05-27 19:34:00 -0700

    a3 Javascript

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -4,13 +4,12 @@ import ModuleControlButtons from "./ModuleControlButtons";
 import { BsGripVertical } from "react-icons/bs";
 import { useParams } from "react-router";
 import { modules } from "../../Database";
-
 export default function Modules() {
     const { cid } = useParams();
     const courseModules = modules.filter((module) => module.course === cid);
     return (
         <div id="wd-modules">
-            <ModulesControls /><br /><br />
+            <ModulesControls /><br /><br /><br /><br />
             <ul id="wd-modules" className="list-group rounded-0">
                 {courseModules.map((module) => (
                     <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
