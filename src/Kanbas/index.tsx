ef761c607cbff55fd2839def4d8c21542c252576
muqingcao <caomuqingy@gmail.com>
2024-06-15 13:15:01 -0700

    a6 commit

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -8,6 +8,9 @@ import { useEffect, useState } from "react";
 import store from "./store";
 import { Provider } from "react-redux";
 import * as client from "./Courses/client";
+import Account from "./Account";
+import Session from "./Account/Session";
+import ProtectedRoute from "./ProtectedRoute";
 
 export default function Kanbas() {
     const [courses, setCourses] = useState<any[]>([]);
@@ -44,7 +47,7 @@ export default function Kanbas() {
             courses.filter((course) => course._id !== courseId)
         );
     };
-    
+
     // Updating a Course
     const updateCourse = async () => {
         await client.updateCourse(course);
@@ -61,33 +64,39 @@ export default function Kanbas() {
 
     return (
         <Provider store={store}>
-            <div id="wd-kanbas" className="h-200">
-                <div className="d-flex h-200">
-                    <div className="d-none d-md-block bg-black">
-                        <KanbasNavigation />
-                    </div>
-                    <div className="flex-fill p-4">
-                        <Routes>
-                            <Route path="/" element={<Navigate to="Dashboard" />} />
-                            <Route path="Account" element={<h1>Account</h1>} />
-                            <Route path="Dashboard" element={
-                                <Dashboard
-                                    courses={courses}
-                                    course={course}
-                                    setCourse={setCourse}
-                                    addNewCourse={addNewCourse}
-                                    deleteCourse={deleteCourse}
-                                    updateCourse={updateCourse} />
-                            } />
-                            <Route path="Courses/:cid/*" element={
-                                <Courses courses={courses} />} />
+            <Session>
+                <div id="wd-kanbas" className="h-200">
+                    <div className="d-flex h-200">
+                        <div className="d-none d-md-block bg-black">
+                            <KanbasNavigation />
+                        </div>
+                        <div className="flex-fill p-4">
+                            <Routes>
+                                <Route path="/" element={<Navigate to="Dashboard" />} />
+                                <Route path="/Account/*" element={<Account />} />
+                                <Route path="Dashboard" element={
+                                    <ProtectedRoute>
+                                        <Dashboard
+                                            courses={courses}
+                                            course={course}
+                                            setCourse={setCourse}
+                                            addNewCourse={addNewCourse}
+                                            deleteCourse={deleteCourse}
+                                            updateCourse={updateCourse} />
+                                    </ProtectedRoute>
+                                } />
+                                <Route path="Courses/:cid/*" element={
+                                    <ProtectedRoute>
+                                        <Courses courses={courses} />
+                                    </ProtectedRoute>} />
 
-                            <Route path="Calendar" element={<h1>Calendar</h1>} />
-                            <Route path="Inbox" element={<h1>Inbox</h1>} />
-                        </Routes>
+                                <Route path="Calendar" element={<h1>Calendar</h1>} />
+                                <Route path="Inbox" element={<h1>Inbox</h1>} />
+                            </Routes>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </Session>
         </Provider>
     )
 };
